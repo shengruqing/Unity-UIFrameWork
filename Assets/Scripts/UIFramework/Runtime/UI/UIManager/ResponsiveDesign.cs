d263f71f320e4a17722db325d2a9fd7ee8d65e7d
shengruqing <shengruqing@live.cn>
2026-04-28 15:10:55 +0800

    优化框架

---
+++ Assets/Scripts/UIFramework/Runtime/UI/UIManager/ResponsiveDesign.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameLogic;
+using UnityEngine;
+
+public class ResponsiveDesign : MonoSingleton<ResponsiveDesign>
+{
+    private ScreenOrientation currentOrientation;
+
+    void Awake()
+    {
+        m_Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        currentOrientation = Screen.orientation;
+    }
+
+
+    public int GetNotchHeight()
+    {
+        var notchWidth = 0;
+        var hige = 0;
+        hige = (int)((Screen.width - Screen.safeArea.width) / 2.0f);
+        if (hige >= 0)
+        {
+            notchWidth = (hige / Screen.width) * 1920;
+        }
+
+        return notchWidth;
+    }
+
+    private void Update()
+    {
+        var newOrientation = Screen.orientation;
+        if (currentOrientation != newOrientation)
+        {
+            currentOrientation = newOrientation;
+            GUIManager.Instance.OnOrientationChanged(currentOrientation);
+        }
+    }
+}
